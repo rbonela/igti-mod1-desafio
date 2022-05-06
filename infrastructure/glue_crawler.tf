71220f18d28cda9b51c4eb963a85a2f27dfda73b
Ramon Bonela <ramonbonela@gmail.com>
2022-05-06 18:40:31 -0300

    Modified glue_crawler.tf file

--- infrastructure/glue_crawler.tf
+++ infrastructure/glue_crawler.tf
@@ -2,7 +2,7 @@ resource "aws_glue_catalog_database" "rais" {
   name = "raisdb"
 }
 
-resource "aws_glue_crawler" "stream" {
+resource "aws_glue_crawler" "rais" {
   database_name = aws_glue_catalog_database.rais.name
   name          = "rais_processing_crawler"
   role          = aws_iam_role.glue_role.arn
